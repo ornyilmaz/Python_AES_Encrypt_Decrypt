8db4109e4101cf6f9153d72eb0c7d393d142fdc7
ORHAN YILMAZ <ornyilmaz@gmail.com>
2023-02-09 17:03:51 +0300

    Add files via upload

---
+++ AES_EncryptDecrypt.py
@@ -0,0 +1,85 @@
+import os
+import sys
+from Crypto.Cipher import AES
+
+def encrypt_file(key, in_filename, out_filename=None, chunksize=64*1024):
+    """ Encrypts a file using AES (CBC mode) with the
+        given key.
+
+        key:
+            The encryption key - a bytes object of length 32.
+
+        in_filename:
+            Name of the input file
+
+        out_filename:
+            If None, '<in_filename>.enc' will be used.
+
+        chunksize:
+            Sets the size of the chunk which the function
+            uses to read and encrypt the file. Larger chunk
+            sizes can be faster for some files and machines.
+            chunksize must be divisible by 16.
+    """
+    if not out_filename:
+        out_filename = in_filename + '.enc'
+
+    iv = os.urandom(16)
+    encryptor = AES.new(key, AES.MODE_CBC, iv)
+    filesize = os.path.getsize(in_filename)
+
+    with open(in_filename, 'rb') as infile:
+        with open(out_filename, 'wb') as outfile:
+            outfile.write(filesize.to_bytes(8, 'big'))
+            outfile.write(iv)
+
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                elif len(chunk) % 16 != 0:
+                    chunk += b' ' * (16 - len(chunk) % 16)
+
+                outfile.write(encryptor.encrypt(chunk))
+
+def decrypt_file(key, in_filename, out_filename=None, chunksize=64*1024):
+    """ Decrypts a file using AES (CBC mode) with the
+        given key.
+
+        key:
+            The encryption key - a bytes object of length 32.
+
+        in_filename:
+            Name of the input file. It should be of the format
+            <filename>.enc
+
+        out_filename:
+            If None, '<in_filename>.dec' will be used.
+
+        chunksize:
+            Sets the size of the chunk which the function
+            uses to read and encrypt the file. Larger chunk
+            sizes can be faster for some files and machines.
+            chunksize must be divisible by 16.
+    """
+    if not out_filename:
+        out_filename = os.path.splitext(in_filename)[0]
+
+    with open(in_filename, 'rb') as infile:
+        origsize = int.from_bytes(infile.read(8), 'big')
+        iv = infile.read(16)
+        decryptor = AES.new(key, AES.MODE_CBC, iv)
+
+        with open(out_filename, 'wb') as outfile:
+            while True:
+                chunk = infile.read(chunksize)
+                if len(chunk) == 0:
+                    break
+                outfile.write(decryptor.decrypt(chunk))
+                outfile.truncate(origsize)
+
+# Example usage
+key = os.urandom(32) # 256-bit key
+print("Key: " + key.hex())
+encrypt_file(key, "uygulama.exe")
+decrypt_file(key, "uygulama.exe.enc")
